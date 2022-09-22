11eb6372100b1d0a92b81bd41d3e34e91f79c0a1
leventeb8 <soros.levente.tech2021d@bolyaimovar.com>
2022-09-22 17:30:06 +0200

    házi

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Kérjük adja meg a kör sugarát:")
+r = float(input())
+
+negy = r*r
+k = 2*3.14*r
+t = 3.14*negy
+d = 2*r
+
+print("A kör kerülete:",k)
+print("A kör területe:",t)
+print("A kör átmérője:",d)
