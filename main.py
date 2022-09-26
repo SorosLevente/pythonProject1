3cdade4f9b639813a8a9e5168c76ecfd36c1cd5f
leventeb8 <soros.levente.tech2021d@bolyaimovar.com>
2022-09-26 18:18:16 +0200

    házi

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
+#kör terület, kerület, átmérő,
 print("Kérjük adja meg a kör sugarát:")
 r = float(input())
 
 negy = r*r
 k = 2*3.14*r
 t = 3.14*negy
+
 d = 2*r
 
-print("A kör kerülete:",k)
-print("A kör területe:",t)
-print("A kör átmérője:",d)
+if r==0:
+    print("Ez neked a kör?!")
+else:
+    print("A kör kerülete:", k, "cm")
+    print("A kör területe:", t, "cm2")
+    print("A körátmérője:", d, "cm")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
